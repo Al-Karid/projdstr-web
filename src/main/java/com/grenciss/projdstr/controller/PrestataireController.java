6f52c4470a8bc07d60d8cf038e2aa783ce3a935a
Gren CISSE Al-Karid <grencisse@gmail.com>
2020-05-03 20:55:32 +0000

    Add prestataire controller

---
+++ src/main/java/com/grenciss/projdstr/controller/PrestataireController.java
@@ -0,0 +1,52 @@
+package com.grenciss.projdstr.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.grenciss.projdstr.model.Prestataire;
+import com.grenciss.projdstr.repository.PrestataireRepository;
+import com.grenciss.projdstr.exception.ResourceNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/prestataires")
+public class PrestataireController {
+    
+    @Autowired
+    PrestataireRepository prestataires;
+
+    @GetMapping("/all")
+    public List<Prestataire> getAllPrestataires(){
+        return prestataires.findAll();
+    }
+
+    @PostMapping("/save")
+    public Prestataire savePrestataire(@Valid @RequestBody Prestataire prestataire){
+        return prestataires.save(prestataire);
+    }
+
+    @PutMapping("/update/{id}")
+    public Prestataire updatePrestataire(@PathVariable(value = "id") Long id, @Valid @RequestBody Prestataire newPrestataire){
+        Prestataire prestataire = prestataires.findById(id).orElseThrow(()-> new ResourceNotFoundException("Prestataire", "id", id));
+        prestataire.update(newPrestataire);
+        return prestataires.save(prestataire);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deletePrestataire(@PathVariable(value="id") Long id){
+        Prestataire prestataire = prestataires.findById(id).orElseThrow(() -> new ResourceNotFoundException("Prestataire", "id", id));
+        prestataires.delete(prestataire);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/grenciss/projdstr/repository/PrestataireRepository.java
@@ -0,0 +1,9 @@
+package com.grenciss.projdstr.repository;
+
+import com.grenciss.projdstr.model.Prestataire;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrestataireRepository extends JpaRepository<Prestataire, Long> {
+    
+}
