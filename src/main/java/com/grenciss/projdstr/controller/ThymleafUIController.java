413597f8790abc93fcc71222ff3fc629d03043af
Gren CISSE Al-Karid <grencisse@gmail.com>
2020-05-05 15:29:37 +0000

    Update form prestataire

--- src/main/java/com/grenciss/projdstr/controller/ThymleafUIController.java
+++ src/main/java/com/grenciss/projdstr/controller/ThymleafUIController.java
@@ -13,7 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller("/dstr")
+@Controller
 public class ThymleafUIController {
     
     @Autowired
