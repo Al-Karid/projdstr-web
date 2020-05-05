87455c441ad2a4f4f1c971d6a9378f6782250731
Gren CISSE Al-Karid <grencisse@gmail.com>
2020-05-05 15:24:55 +0000

    Prestataire form

--- src/main/java/com/grenciss/projdstr/controller/ThymleafUIController.java
+++ src/main/java/com/grenciss/projdstr/controller/ThymleafUIController.java
@@ -1,13 +1,37 @@
 package com.grenciss.projdstr.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.validation.Valid;
 
-@Controller
+import com.grenciss.projdstr.exception.ResourceNotFoundException;
+import com.grenciss.projdstr.model.Prestataire;
+import com.grenciss.projdstr.repository.PrestataireRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller("/dstr")
 public class ThymleafUIController {
     
+    @Autowired
+    PrestataireRepository prestataires;
+
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/nouveau/prestataire")
+    public String newPrestataireForm(Prestataire prestataire){
+        return "form-prestataire";
+    }
+
+    @PostMapping("/save/prestataire")
+    public String savePrestataire(@Valid Prestataire prestataire, BindingResult result, Model model){
+        prestataires.save(prestataire);
+        return "redirect:/";
     }
 }
