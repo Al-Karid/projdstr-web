d5acdb534ff377f61feabd2462a2d45c7e0a3e4d
Gren CISSE Al-Karid <grencisse@gmail.com>
2020-05-10 13:28:19 +0000

    Default test data by cmd runner

--- src/main/java/com/grenciss/projdstr/ProjdstrApplication.java
+++ src/main/java/com/grenciss/projdstr/ProjdstrApplication.java
@@ -1,7 +1,14 @@
 package com.grenciss.projdstr;
 
+import com.grenciss.projdstr.model.Etude;
+import com.grenciss.projdstr.model.Prestataire;
+import com.grenciss.projdstr.repository.EtudeRepository;
+import com.grenciss.projdstr.repository.PrestataireRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjdstrApplication {
@@ -10,4 +17,31 @@ public class ProjdstrApplication {
 		SpringApplication.run(ProjdstrApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadEtudeData(EtudeRepository etudes, PrestataireRepository prestataires){
+		return (args) -> {
+			// Save new Etude
+			Etude etude = new Etude();
+			etude.setLibelle("Achat de poulet");
+			etude.setBudget(15000);
+			etudes.save(etude);
+
+			// Save Prestataire
+			Prestataire prestataire = new Prestataire();
+			prestataire.setLibelle("Agrics");
+			prestataire.setEmail("agrics.sa@newcorp.com");
+			prestataire.setSiege("Sassandra");
+			prestataire.setPersAContacter1("San Alexys");
+			prestataire.setContact1("+22554849532");
+			prestataires.save(prestataire);
+
+			Prestataire p = new Prestataire();
+			p.setLibelle("FineTechs");
+			p.setEmail("world.fine@finetechs.com");
+			p.setSiege("Abobo Dockui");
+			p.setPersAContacter1("Sahiri Samuel");
+			p.setContact1("+22569321400");
+			prestataires.save(p);
+		};
+	}
 }
